fe899f13ead9226ad1e6ee225b45661afd8fa929
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-09-16 21:48:03 -0600

    [Add] - Workshop 2. Static Library

---
+++ Workshop2 - GCC/lmath/lmath.h
@@ -0,0 +1,14 @@
+#ifndef _L_MATH_H
+#define _L_MATH_H
+
+#include <math.h>
+
+
+double add(double num1, double num2);
+double substract(double num1, double num2);
+double mult(double num1, double num2);
+double div(double num1, double num2);
+double sqrt(double num);
+
+
+#endif /* _L_MATH_H */
