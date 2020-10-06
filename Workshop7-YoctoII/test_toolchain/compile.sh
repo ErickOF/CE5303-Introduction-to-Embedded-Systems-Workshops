0355478888a548978d6178365789658786fc8d92
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-10-05 21:52:04 -0600

    [Add] - Workshop7 - Yocto II

---
+++ Workshop7-YoctoII/test_toolchain/compile.sh
@@ -0,0 +1,8 @@
+# Set environment
+. /opt/poky/3.1.2/environment-setup-cortexa7t2hf-neon-vfpv4-poky-linux-gnueabi
+
+# Test command
+#echo $CC
+
+# Compile
+$CC -o sayhello helloworld.c
---
+++ Workshop7-YoctoII/test_toolchain/config.sh
@@ -0,0 +1,5 @@
+# Set environment
+. /opt/poky/3.1.2/environment-setup-cortexa7t2hf-neon-vfpv4-poky-linux-gnueabi
+
+# Test command
+echo $CC
